18a34a88ce530ac664e2fdb8f82425b6ed1cc4fe
mumat0103 <69793985+mumat0103@users.noreply.github.com>
2020-08-19 17:57:49 +0900

    Update 2438번(별 찍기 - 1).cpp

--- 2438번(별 찍기 - 1).cpp
+++ 2438번(별 찍기 - 1).cpp
@@ -7,12 +7,7 @@ int main() {
 	cin >> n;
 	for(int i = 1; i <= n; i++) {
 		for(int j = 1; j <= i; j++)
-			if(j <= n - i) {
-				cout << " ";
-			} else {
-				cout << "*" << "";
-			}
-			
+			cout << "*" << "";
 	cout << "\n";
 	}
 }
